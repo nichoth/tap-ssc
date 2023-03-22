a7000552db25be26896316d328ee2da4fe3139ad
nick thomas <nichoth@gmail.com>
2023-03-22 09:51:38 -0700

    rm buffer

--- buffer.js
+++
@@ -1,9 +0,0 @@
-/**
- * @module Buffer
- *
- * @see {@link https://nodejs.org/api/buffer.html#buffer_class_buffer}
- */
-
-import { Buffer } from 'buffer'
-export * from 'buffer'
-export default Buffer
